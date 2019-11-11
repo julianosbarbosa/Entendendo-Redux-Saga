1c916e53ab662ee4d352d5c6ef72ab6791ec2159
Juliano <julianotrabalho2016@gmail.com>
2019-11-11 12:39:02 -0300

    UPDATE - actions creators add todo list

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
+import { bindActionCreators } from "redux";
+import * as actionsTodo from "./store/actions/todos";
 
 const TodoList = ({ todos, addTodo }) => {
   return (
@@ -29,9 +31,8 @@ const mapStateToProps = state => ({
   todos: state.todos
 });
 
-const mapDispatchToProps = dispatch => ({
-  addTodo: text => dispatch({ type: "ADD_TODO", payload: { text } })
-});
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(actionsTodo, dispatch);
 
 export default connect(
   mapStateToProps,
